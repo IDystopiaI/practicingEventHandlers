eae6f08419650f7815a19bb92e8d967ef3822806
IDystopiaI <dylan.london94@gmail.com>
2024-01-08 11:13:56 -0800

    basic js element selection examples

---
+++ website/index.js
@@ -0,0 +1,22 @@
+// print to browser console
+// console.log("Hello world");
+// console.log("Lorem ipsum dolor sit, amet consectetur");
+
+// create an alert prompt
+// window.alert("This is an alert!")
+// window.alert("This is an alert!")
+
+// set textcontent. Overwrites existing content in an element.
+document.getElementById("myH1").textContent = "Hello";
+
+// Append text. Appends text to existing content.
+// document.getElementById("myH1").append("Hello world");
+
+document.getElementById("myP").textContent = "I like pizza";
+
+// single line comment
+
+/* multi
+line
+comment */
+
