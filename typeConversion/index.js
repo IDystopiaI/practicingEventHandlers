31664ca4e4736ea8c2c0909542ab4384763fb67e
IDystopiaI <dylan.london94@gmail.com>
2024-01-08 22:14:08 -0800

    type conversion example

---
+++ typeConversion/index.js
@@ -0,0 +1,26 @@
+// let age = window.prompt("Enter your age: ");
+
+// user input is a string, results in concatenation
+// age of 20 -> 201 after concatenation
+// age +=1;
+
+/* example showing user input is taken as a string
+// can not use augmented assignment here
+age = Number(age) + 1;
+
+// age += 1;
+
+console.log(age, typeof age);
+*/
+
+let x;
+let y;
+let z;
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
