0c66df939a62e0573b2e9d3c91d92be07371d606
IDystopiaI <dylan.london94@gmail.com>
2024-02-23 15:57:04 -0800

    started work with jquery examples

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    // removes warning for '$' undefiend when working with jQuery
+    // https://stackoverflow.com/a/39511584
+    jquery: true,
+  },
+  extends: "eslint:recommended",
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    // https://stackoverflow.com/a/71384607
+    // note you must disable the base rule
+    // as it can report incorrect error
+
+    "no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+      },
+    ],
+  },
+};
---
+++ jquery/example/index.js
@@ -0,0 +1,26 @@
+// Syntax for jQuery
+// $(selector).action()
+
+// use a document.ready event to wrap your code
+// this prevents js from executing before the DOM has finished loading
+// on button click hide all p elements
+
+// $(document).ready(function () {
+//   $("button").click(function () {
+//     $("p").hide();
+//   });
+// });
+
+// on button click hide element with id="cereal"
+// $(document).ready(function () {
+//   $("button").click(function () {
+//     $("#cereal").hide();
+//   });
+// });
+
+// select a .class
+$(document).ready(function () {
+  $("button").click(function () {
+    $(".unicycle").hide();
+  });
+});
